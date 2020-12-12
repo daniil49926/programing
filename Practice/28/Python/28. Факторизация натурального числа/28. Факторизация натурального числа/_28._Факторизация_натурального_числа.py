ac47adca42d59d72a10d8822bf601c41f32f8d12
daniil49926 <daniil49926@gmail.com>
2020-12-12 14:57:50 +0300

    edit 28 py

--- Practice/28/Python/28. Факторизация натурального числа/28. Факторизация натурального числа/_28._Факторизация_натурального_числа.py
+++ Practice/28/Python/28. Факторизация натурального числа/28. Факторизация натурального числа/_28._Факторизация_натурального_числа.py
@@ -1,12 +1,31 @@
 
-import math
-from collections import Counter
+def sum(x, y):
+	for key, value in y.items():
+		if key in x:
+			x[key] += value
+		else:
+			x[key] = value
+	return x
 
-number=int(input())
-for i in range(2, int(math.sqrt(number)) + 1):
-    while (number % i == 0):
-         print(i)
-         number //= i 
-if (number != 1): 
-    print(number)
+def fact(n):
+	for i in range(2, n):
+		if (n % i == 0):
+			return sum(fact(i), fact(n // i))
+	return {
+		n:1
+		} 
 
+def factor(n: int) -> None:
+	temp = fact(n)
+	for i in range(len(temp.items())):
+		value = list(temp.values())[i]
+		key = list(temp.keys())[i]
+		print(key, end='');
+		if (value > 1):
+			print('^', value, sep='', end='')
+		if i+1 < len(temp.items()): print('*', end='')
+	
+
+x = int(input())
+factor(x)
+print ('\n')
