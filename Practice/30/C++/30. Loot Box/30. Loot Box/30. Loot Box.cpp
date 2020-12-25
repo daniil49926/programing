2988be135b414d752764fcc8b113e2c75f0f1b1e
daniil49926 <daniil49926@gmail.com>
2020-12-25 13:57:16 +0300

    pair commit

---
+++ Practice/30/C++/30. Loot Box/30. Loot Box/30. Loot Box.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+struct Item
+{
+	struct coin
+	{
+		int count;
+	};
+	struct weapon
+	{
+		int damage;
+		int critical;
+		int durability;
+	};
+	struct rune
+	{
+		int level;
+		
+	};
+	struct armor
+	{
+		int guard;
+		int durability;
+	};
+};
+
+void main() {
+
+}
