1c23a538f9e79c8bef054a0af76990e35c7d7412
daniil49926 <daniil49926@gmail.com>
2021-03-06 17:22:47 +0300

    add 34,35

---
+++ Practice/34/C++/34. Одномерный динамический массив/34. Одномерный динамический массив/34. Одномерный динамический массив.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+
+void sort_arr(int* arr, int len) {
+    for (int i = 1; i < len; i++) {
+        int temp = arr[i];
+        int value = i - 1;
+        while (value >= 0 && arr[value] > temp) {
+            arr[value + 1] = arr[value];
+            arr[value] = temp;
+            value--;
+        }
+    }
+}
+
+int* create_arr(int len, int start = 0, int step = 0){
+    int* arr = new int[len];
+    for (int i = 0; i < len; i++){
+        arr[i] = start;
+        start += step;
+    }
+    return arr;
+}
+
+void create_arr(int** arr, int len, int start, int step){ *arr = create_arr(len, start, step); }
+
+void del_arr(int** arr){
+    if (arr == nullptr){
+        return;
+    }
+    delete[] * arr;
+    *arr = nullptr;
+}
+
+int* print(int* arr, int size){
+    std::cout << '[';
+    for (int i = 0; i < size; i++){
+        std::cout << arr[i];
+        if (i < size - 1){
+            std::cout << ", ";
+        }
+    }
+    std::cout << ']' << std::endl;
+    return arr;
+}
+
+void main(){
+    setlocale(LC_ALL, "ru");
+    int length_arr; 
+    int start_point; 
+    int step;
+
+    std::cout << "Введите длину: ";
+    std::cin >> length_arr;
+    std::cout << "Введите начальную точку: ";
+    std::cin >> start_point;
+    std::cout << "Введите шаг: ";
+    std::cin >> step;
+    int* arr;
+    create_arr(&arr, length_arr, start_point, step);
+    sort_arr(arr, length_arr);
+    print(arr, length_arr);
+    del_arr(&arr);
+}
---
+++ Practice/35/C++/35. Структура — массив целых чисел/35. Структура — массив целых чисел/35. Структура — массив целых чисел.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+
+struct IntArray {
+    int* data;
+    int size;
+};
+
+void create(IntArray& arr, const int len){
+    arr.data = new int[len];
+    arr.size = len;
+}
+
+void create(IntArray* arr, const int size){
+    create(*arr, size);
+}
+
+int get(IntArray& arr, const int index){
+    return arr.data[index];
+}
+
+int get(IntArray* arr, const int index){
+    return get(*arr, index);
+}
+
+void set(IntArray& arr, const int index, const int value){
+    arr.data[index] = value;
+}
+
+void set(IntArray* arr, const int index, const int value){
+    set(*arr, index, value);
+}
+
+void print(IntArray& arr){
+    std::cout << '[';
+    for (int i = 0; i < arr.size; i++){
+        std::cout << arr.data[i];
+        if (i < arr.size - 1){
+            std::cout << ", ";
+        }
+    }
+    std::cout << ']' << std::endl;
+}
+
+void print(IntArray* arr){
+    print(*arr);
+}
+
+void resize(IntArray& arr, const int newSize){
+    if (newSize > arr.size){
+        int* newArr = new int[newSize];
+        memcpy(newArr, arr.data, sizeof(int) * arr.size);
+        for (int i = arr.size; i < newSize; ++i){
+            newArr[i] = 0;
+        }
+        delete[] arr.data;
+        arr.data = newArr;
+        arr.size = newSize;
+        return;
+    }
+    int* newArr = new int[newSize];
+    memcpy(newArr, arr.data, sizeof(int) * newSize);
+    delete[] arr.data;
+    arr.data = newArr;
+    arr.size = newSize;
+}
+
+void resize(IntArray* arr, const int newSize){
+    resize(*arr, newSize);
+}
+
+void destroy(IntArray& arr){
+    delete[] arr.data;
+    arr.data = nullptr;
+    arr.size = 0;
+}
+
+void destroy(IntArray* arr){
+    destroy(*arr);
+}
+
+int main(){
+    int len = 30;
+    IntArray arr;
+    create(arr, len);
+    for (int i = 0; i < len; i++){
+        set(arr, i, i + 1);
+    }
+    print(arr);
+    resize(arr, 50);
+    print(arr);
+    resize(arr, 10);
+    print(arr);
+    destroy(arr);
+}
